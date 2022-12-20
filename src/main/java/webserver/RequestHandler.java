2382ecfb0ad6cfe8f8b10e082fc0cf6d95a79e78
daduPark <koreatkdtn@gmail.com>
2022-12-20 22:18:36 +0900

    3.4.4.1 요구사항 1 - index.html 응답하

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,17 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +30,24 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+        	InputStreamReader isr = new InputStreamReader(in);
+        	BufferedReader br = new BufferedReader(isr);
+
+        	String firstLine = br.readLine();
+        	
+        	String findUrl = firstLine.split(" ")[1];
+
+        	byte[] body ;
+        	if(findUrl.equals("/index.html")) {
+        		body = Files.readAllBytes(new File("./webapp" + findUrl).toPath());
+        	}else {
+        		body = "Hello SAN".getBytes();
+        	}
+        	
+        	// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Sangsu".getBytes();
+            
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
