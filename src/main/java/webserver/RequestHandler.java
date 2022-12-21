dabdd4a0dd2d6ab79705d8e4eb85833ac7d877b0
DaduPark <koreatkdtn@gmail.com>
2022-12-22 07:21:46 +0900

    3.4.3.2 요구사항2- get방식으로 회원가입하

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,10 +8,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -29,12 +32,48 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
  
         	String mappingUrl = getMappingUrl(getHeaderFirstLine(in));
-        	byte[] body = getMappingBody(getMappgingFile(mappingUrl));
+                    
+        	if(mappingUrl.contains("/create")) {
+        		int splitIndex = mappingUrl.indexOf("?");
+        		String userInfoArray[] = mappingUrl.substring(splitIndex).split("&");
+        		
+        		String userId ="";
+                String password ="";
+                String name ="";
+                String email ="";
+                
+        		for(String userInfo : userInfoArray) {
+        			String userInfoMap[] = userInfo.split("=");
+        			
+        			userInfoMap[1] = URLDecoder.decode(userInfoMap[1], "UTF-8");
+        			if("userId".contains(userInfoMap[0])) {
+        				userId = userInfoMap[1];
+        			}
+        			
+        			if("password".contains(userInfoMap[0])) {
+        				password = userInfoMap[1];
+        			}
+        			
+        			if("name".contains(userInfoMap[0])) {
+        				name = userInfoMap[1];
+        			}
+        			
+        			if("email".contains(userInfoMap[0])) {
+        				email = userInfoMap[1];
+        			}
+        		}
+        		User user = new User(userId, password, name, email);
+        		
+        		System.out.println(user.toString());
+        	}else {
+        		byte[] body = getMappingBody(getMappgingFile(mappingUrl));
+            	
+            	// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        		DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+        	}
         	
-        	// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-    		DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
